a40d51bb005b1ebcb6b24686ead2f6b4981cf126
Allovallo <grossco@meta.ua>
2024-11-08 18:21:57 +0200

    files downdated

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,3 @@
-import { Layout } from "./Layout/Layout";
-import { TaskList } from "./TaskList/TaskList";
-import { TaskForm } from "./TaskForm/TaskForm";
-import { AppBar } from "./AppBar/AppBar";
-
 export const App = () => {
-  return (
-    <Layout>
-      <AppBar />
-      <TaskForm />
-      <TaskList />
-    </Layout>
-  );
+  return "App";
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <Provider>
     <App />
   </Provider>
 );
