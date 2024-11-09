441c16a493c8853e52965814e58c699bd5a1d317
Allovallo <grossco@meta.ua>
2024-11-09 11:04:39 +0200

    files updated

--- src/index.js
+++ src/index.js
@@ -2,10 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App";
-import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider>
-    <App />
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
