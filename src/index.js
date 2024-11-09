e79e12632e0bed0be8aad3965970ba849d17c85f
Allovallo <grossco@meta.ua>
2024-11-09 18:23:18 +0200

    files updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./components/App";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
