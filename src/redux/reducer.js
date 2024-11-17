edaf507905cc14fad4bdae1cd9be04367ef6cf1e
Allovallo <grossco@meta.ua>
2024-11-17 09:17:16 +0200

    files updated

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,20 +1,15 @@
 import { combineReducers } from "redux";
 import { statusFilters } from "./constants";
 
-const initialState = {
-  tasks: [
-    { id: 0, text: "Learn HTML and CSS", completed: true },
-    { id: 1, text: "Get good at JavaScript", completed: true },
-    { id: 2, text: "Master React", completed: false },
-    { id: 3, text: "Discover Redux", completed: false },
-    { id: 4, text: "Build amazing apps", completed: false },
-  ],
-  filters: {
-    status: statusFilters.all,
-  },
-};
+const tasksInitialState = [
+  { id: 0, text: "Learn HTML and CSS", completed: true },
+  { id: 1, text: "Get good at JavaScript", completed: true },
+  { id: 2, text: "Master React", completed: false },
+  { id: 3, text: "Discover Redux", completed: false },
+  { id: 4, text: "Build amazing apps", completed: false },
+];
 
-const tasksReducer = (state = initialState, action) => {
+const tasksReducer = (state = tasksInitialState, action) => {
   switch (action.type) {
     case "tasks/addTask":
       return [...state, action.payload];
