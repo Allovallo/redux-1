391ae024b5e5775a7a57888651fdd9507843bcc9
Allovallo <grossco@meta.ua>
2024-11-19 17:58:32 +0200

    files updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
-import { TaskForm } from "./TaskForm/TaskForm";
-import { AppBar } from "./AppBar/AppBar";
-import { Layout } from "./Layout/Layout";
-import { TaskList } from "./TaskList/TaskList";
+// import { TaskForm } from "./TaskForm/TaskForm";
+// import { AppBar } from "./AppBar/AppBar";
+// import { Layout } from "./Layout/Layout";
+// import { TaskList } from "./TaskList/TaskList";
+
+// export const App = () => {
+//   return (
+//     <Layout>
+//       <AppBar />
+//       <TaskForm />
+//       <TaskList />
+//     </Layout>
+//   );
+// };
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTasks } from "../redux/operations";
+import { getTasks } from "../redux/selectors";
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const { items, isLoading, error } = useSelector(getTasks);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
-    <Layout>
-      <AppBar />
-      <TaskForm />
-      <TaskList />
-    </Layout>
+    <div>
+      {isLoading && <b>Loading tasks...</b>}
+      {error && <b>{error}</b>}
+      <p>{items?.length > 0 && JSON.stringify(items, null, 2)}</p>
+    </div>
   );
 };
