7c609d0ad518b4e3dfd4e3b683f238ce34cc7aa1
Allovallo <grossco@meta.ua>
2024-11-24 14:33:12 +0200

    files updated

--- src/components/TaskCounter/TaskCounter.js
+++ src/components/TaskCounter/TaskCounter.js
@@ -1,26 +1,15 @@
 import { useSelector } from "react-redux";
-import { selectTasks } from "../../redux/selectors";
+import { selectTaskCount } from "../../redux/selectors";
+// import { selectTasks } from "../../redux/selectors";
 import css from "./TaskCounter.module.css";
 
 export const TaskCounter = () => {
-  const tasks = useSelector(selectTasks);
-
-  const count = tasks.reduce(
-    (acc, task) => {
-      if (task.completed) {
-        acc.completed += 1;
-      } else {
-        acc.active += 1;
-      }
-      return acc;
-    },
-    { active: 0, completed: 0 }
-  );
+  const { active, completed } = useSelector(selectTaskCount);
 
   return (
     <div>
-      <p className={css.text}>Активні: {count.active}</p>
-      <p className={css.text}>Завершені: {count.completed}</p>
+      <p className={css.text}>Активні: {active}</p>
+      <p className={css.text}>Завершені: {completed}</p>
     </div>
   );
 };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,6 @@
+import { statusFilters } from "./constants";
+import { createSelector } from "@reduxjs/toolkit";
+
 export const selectTasks = (state) => state.tasks.items;
 
 export const selectIsLoading = (state) => state.tasks.isLoading;
@@ -5,3 +8,35 @@ export const selectIsLoading = (state) => state.tasks.isLoading;
 export const selectError = (state) => state.tasks.error;
 
 export const selectStatusFilter = (state) => state.filters.status;
+
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    console.log("Підраховую завдання... мемоізуємо!");
+
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter((task) => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter((task) => task.completed);
+      default:
+        return tasks;
+    }
+  }
+);
+
+export const selectTaskCount = createSelector([selectTasks], (tasks) => {
+  console.log("Підраховую завдання... мемоізуємо!");
+
+  return tasks.reduce(
+    (count, task) => {
+      if (task.completed) {
+        count.completed += 1;
+      } else {
+        count.active += 1;
+      }
+      return count;
+    },
+    { active: 0, completed: 0 }
+  );
+});
